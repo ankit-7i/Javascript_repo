161854644605a85370deb6bf19a4f2a21514398b
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-03-29 17:06:25 +0000

    conversionOperation

--- Javascript_Basics/03_conversionOperation.js
+++ Javascript_Basics/03_conversionOperation.js
@@ -26,6 +26,70 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+//*************************** Operations ******************************
+let value = 3
+let negvalue = - value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Ankit"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+//exception 
+// It follows the ecma script guide line 
+console.log(2+2+"1");
+
+
+console.log((3+4)*5%3);
+
+// These above code are bad practice as a developer 
+
+//Tricky conversion
+console.log(+true);
+console.log(+"");
+
+// ASSIGNMENT OPERATOR
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1 = 200
+++gameCounter1;
+console.log(gameCounter1);
+// referances
+//https://tc39.es/ecma262/#sec-abstract-operations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
