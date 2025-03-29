07aa57045fe1d138b1b7c3d460b9291f22e39872
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-03-29 14:09:07 +0000

    updated

---
+++ Javascript_Basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello") we are using nodejds,not browser
+// code readability should be high 
+console.log(3+3); 
+console.log("Ankit");
+
+let name = "ankit"
+let age = 23
+let isLoggedIn = true
+
+// number ==> 2 to power 53
+// bigint
+// string ==>""
+// boolean ==> true/false
+// null ==> standalone value 
+// undefined ==> 
+// symbol ==> unique 
+
+/*  
+object 
+*/
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
