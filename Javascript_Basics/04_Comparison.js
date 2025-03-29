f96d0f990b262fccc3c1063464234b20f6af9852
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-03-29 17:22:32 +0000

    Comparison

---
+++ Javascript_Basics/04_Comparison.js
@@ -0,0 +1,34 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2 != 1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// above are some bad practice of coding and  raise confusion and it is not clean code practice 
+// strict check 
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+
