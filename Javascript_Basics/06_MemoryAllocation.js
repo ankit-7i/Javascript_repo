c887e2e0490e5c15f79e68d296fb4c4939f30949
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-03-30 04:50:41 +0000

    Memory allocation in js

---
+++ Javascript_Basics/06_MemoryAllocation.js
@@ -0,0 +1,26 @@
+// stack memory and Heap memory 
+
+// primitive datatype allocate in Stack memory
+// Non-primitive allocate in Heap memory
+let myInstaname="Ankit_Rout_7_"
+
+let anothername = myInstaname
+
+anothername="Ankit_7_"
+
+console.log(myInstaname);
+
+console.log(anothername);
+
+let userOne ={
+    email:"ankit07@gmail.com",
+    upi:"ankit07@ybl"
+}
+
+let userTwo= userOne
+
+userTwo.email ="ankit007@hotmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
