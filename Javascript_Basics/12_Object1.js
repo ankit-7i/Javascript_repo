4e5008678c206c874bded306aa80f40690e06901
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-03-31 18:38:38 +0000

    Object in js

--- Javascript_Basics/12_Object1.js
+++ Javascript_Basics/12_Object1.js
@@ -1,8 +1,12 @@
 // singleton
 // object literals
+const mySym = Symbol("key1")
 
 const Jsuser = {
     name:"Ankit",
+    "fullname":"Ankit Rout",
+    mySym:"MyKey",
+    [mySym]:"keysuper",
     age:23,
     location:"Bhubaneswar",
     email:"2370056@kiit.ac.in",
@@ -11,5 +15,34 @@ const Jsuser = {
 }
 console.log(Jsuser.email);
 console.log(Jsuser["email"]);
+console.log(Jsuser["fullname"]);
+console.log(Jsuser.mySym);
+console.log(typeof Jsuser.mySym);
+console.log(Jsuser[mySym]);
+
+Jsuser.email = "ankit513151979@gmail.com"
+//Object.freeze(Jsuser)
+Jsuser.email="ank07@gmail.com"
+console.log(Jsuser);
+
+
+Jsuser.greeting = function(){
+    console.log("Hello Jsuser");    
+}
+
+Jsuser.greeting2 = function(){
+    console.log(`javascript dev,${this.name}`);
+
+}
+console.log(Jsuser.greeting);
+console.log(Jsuser.greeting());
+console.log(Jsuser.greeting2());
+
+
+
+
+
+
+
 
 
