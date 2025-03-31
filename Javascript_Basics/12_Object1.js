cbbb5684748a001dc746da98c5f3653d2b11179d
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-03-31 18:22:19 +0000

    Object in js

---
+++ Javascript_Basics/12_Object1.js
@@ -0,0 +1,15 @@
+// singleton
+// object literals
+
+const Jsuser = {
+    name:"Ankit",
+    age:23,
+    location:"Bhubaneswar",
+    email:"2370056@kiit.ac.in",
+    isLoggedIn:true,
+    lastLoginDays:["Monday","Tuesday"]
+}
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+
+
