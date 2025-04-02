acd2b3a292cc54e1b9a82c9021cd02b0c7928300
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-04-02 20:10:20 +0000

    Object de struturing and intro to JSON

--- Javascript_Basics/14_Object-deStr_JSON.js
+++ Javascript_Basics/14_Object-deStr_JSON.js
@@ -1,1 +1,22 @@
-console.log("Object_De_str tuto");
+const course = {
+    coursename :"Mern stack development",
+    price:"5999",
+    courseInstrutor:"Ankit"
+}
+const {courseInstrutor} =  course
+console.log(courseInstrutor);
+
+const {price : fees} =course // de struturing in js 
+
+console.log(fees);
+
+//JSON api small concept
+/*
+{
+    "name":"Ankit",
+    "cname":"Mean stack dev",
+    "fees":"5999"
+}
+JSON Format 
+*/
+//JSON FORMATOR & Random User gen
