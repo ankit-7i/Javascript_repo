8cb067c488109c96cbc50974df1be496f39f195e
Ankit Rout <140189053+ankit-7i@users.noreply.github.com>
2025-04-06 02:58:16 +0000

    immediate invoked Function expression

--- Javascript_Basics/18_IIFE.js
+++ Javascript_Basics/18_IIFE.js
@@ -1,1 +1,17 @@
-console.log("hi welcome to IIFE");
+// Immediately Invoked Function Expressions (IIFE)
+function coffee(){
+    console.log(`DB CONNECTION`);
+}
+//coffee()
+(function faluda(){
+    // named FE
+    console.log(`faluda order successful`);
+    
+})();
+// to reduce global scope polllution we use FE
+((name) =>{
+    //simple FE with parameter
+    console.log(`Hi bro i am ${name}`);
+    
+})('ankit');
+
